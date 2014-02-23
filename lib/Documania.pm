3a684ece93dc7af6472fb75b6d0193af966ab6f0
moznion <moznion@gmail.com>
2014-02-23 20:24:49 +0900

    Initial commit

---
+++ lib/Documania.pm
@@ -0,0 +1,39 @@
+package Documania;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Documania - It's new $module
+
+=head1 SYNOPSIS
+
+    use Documania;
+
+=head1 DESCRIPTION
+
+Documania is ...
+
+=head1 LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+moznion E<lt>moznion@gmail.comE<gt>
+
+=cut
+
