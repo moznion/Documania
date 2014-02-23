27ee312ba65d4d2d2b49b67debaa9514b0fb580b
moznion <moznion@gmail.com>
2014-02-23 23:28:37 +0900

    Implement basic functions

---
+++ lib/Documania/Dispatcher/Router/Boom.pm
@@ -0,0 +1,28 @@
+package Documania::Dispatcher::Router::Boom;
+use strict;
+use warnings;
+use utf8;
+use Router::Boom::Method;
+
+sub analyze_routes {
+    my ($class, $dispatcher_class) = @_;
+
+    no warnings 'redefine';
+    my $orig = Router::Boom::Method->can('add');
+    my ($packages, $routes);
+    local *Router::Boom::Method::add = sub {
+        my ($self, $method, $path, $stuff) = @_;
+        if ($path =~ m{api}) {
+            push @$packages, $stuff->[0];
+            push @{$routes->{$stuff->[0]}}, [$method, $path, $stuff];
+        }
+        $orig->(@_);
+    };
+
+    eval "require $dispatcher_class"; ## no critic
+
+    return ($packages, $routes);
+}
+
+1;
+
